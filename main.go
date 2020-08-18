3e70b8ef4ad6428b001c8355d8e1de75deccfae8
Баев Никита <nikita-baev@yandex.ru>
2020-08-18 19:44:41 +0300

    Add RSelect algo

--- main.go
+++ main.go
@@ -6,11 +6,34 @@ import (
 	"time"
 )
 
+// Sort 64 elements array. Pivot element choice
+//last: n=100 comp=180
+//first: n=100 comp=180
+//rand: n=100 comp=180
+
+// RSelect 64 element array comparisons
+//index=5: n=100 comp=70
+//index=60: n=100 comp=66
+//index=32: n=100 comp=94
 func main() {
-	// Pivot element choice
-	//last n=100 comp=180
-	//first n=100 comp=180
-	//rand n=100 comp=180
+	rselect()
+}
+
+func rselect() {
+	n := 100
+	comp := 0
+	for i := 0; i < n; i++ {
+		rand.Seed(time.Now().UnixNano())
+		for i := 0; i < n; i++ {
+			a := rand.Perm(64)
+			_, c := quicksort.RSelect(a, 32)
+			comp += c
+		}
+	}
+	println(comp/(n*n))
+}
+
+func sort()  {
 	n := 100
 	comp := 0
 	for i := 0; i < n; i++ {
--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -30,6 +30,34 @@ func sort (arr []int, left int, right int, count chan int) {
 	sort(arr, j + 1, right, count)
 }
 
+func RSelect (arr[] int, index int) (int, int) {
+	count := make(chan int, 1000)
+	res := rselect(arr, 0, len(arr) - 1, index, count)
+	comp := 0
+	close(count)
+	for val := range count {
+		comp += val
+	}
+	return res, comp
+}
+
+func rselect (arr []int, left int, right int, index int, count chan int) int {
+	if left >= right {
+		return arr[right]
+	}
+	i := pivotRand(left, right) // random
+	arr[left], arr[i] = arr[i], arr[left]
+	j := partition(arr, left, right, count)
+	if j == index {
+		return arr[j]
+	}
+	if j > index {
+		return rselect(arr, left, j - 1, index, count)
+	} else {
+		return rselect(arr, j + 1, right, index, count)
+	}
+}
+
 func partition(arr []int, left int, right int, count chan int) int {
 	p := arr[left]
 	i := left + 1
